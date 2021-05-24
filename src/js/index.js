e754aa5a39a26d43171821012b17503d8599b2ef
ShylaZou <71455534+ShylaZou@users.noreply.github.com>
2021-05-24 13:08:43 -0400

    okay.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,24 @@
-import { DOMSelectors } from "./DOM.js";
+import { DOMSelectors } from "./DOM";
 
 const init = function () {
   let championValue;
-  const apikey = `RGAPI-7bda1256-e934-4de6-b042-d6f46096486b`;
+  let summonerValue;
+  const apikey = `RGAPI-525e148a-73f3-4456-aa42-2f029ef0ace6`;
+
+  const grabData = async function (query) {
+    try {
+      const response = await fetch(query);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+      alert("something went wrong");
+    }
+  };
+};
+
+const displayData = async function () {
+  let query;
+  console.log(championValue);
 };
